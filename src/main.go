94487c676f81b087db524fc07ba48e730cc5c66a
andjrue <drewsimcox12@gmail.com>
2024-06-21 16:52:27 -0400

    Complete, project working

--- src/main.go
+++ src/main.go
@@ -6,20 +6,22 @@ import (
 	"time"
 )
 
-const numPhil int = 5
-const food = 500
-const maxSleepTime = 1
+const numPhil int = 10
+const maxSleepTime = 50
 
-var philosophers = [5]int{1, 2, 3, 4, 5}
+var food = 500
+var foodMutex sync.Mutex
+var m = make(map[int]int)
 
 type Philosopher struct {
 	id                  int
-	leftFork, rightFork chan struct{}
+	leftFork, rightFork *sync.Mutex
+	mu                  sync.Mutex
 }
 
 func (p *Philosopher) think() {
-	fmt.Printf("Philosopher %d is thinking", p.id)
-	time.Sleep(time.Duration(maxSleepTime) * time.Second)
+	fmt.Printf("Philosopher %d is thinking \n", p.id)
+	time.Sleep(time.Duration(maxSleepTime) * time.Millisecond)
 }
 
 func getForks(leftFork, rightFork chan struct{}) bool {
@@ -45,17 +47,62 @@ func (p *Philosopher) eatFood(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for {
-		if getForks(p.leftFork, p.rightFork) {
-			fmt.Printf("Philosopher %d is eating.\n", p.id)
-			time.Sleep(time.Duration(maxSleepTime) * time.Second)
+		foodMutex.Lock()
+		p.leftFork.Lock()
+		p.rightFork.Lock()
+
+		if food > 0 {
+			fmt.Printf("Philosopher %d is eating. \n", p.id)
+			food--
+			m[p.id]++
+			fmt.Printf("Food Remaining: %d \n", food)
+			foodMutex.Unlock()
+
+			time.Sleep(time.Duration(maxSleepTime) * time.Millisecond)
 			p.think()
+		} else {
+			foodMutex.Unlock()
+			p.rightFork.Unlock()
+			p.leftFork.Unlock()
 			return
 		}
+
+		p.rightFork.Unlock()
+		p.leftFork.Unlock()
 	}
 }
 
-func main() {
+func createPhilArr(numPhil int) []*Philosopher {
+	forks := make([]*sync.Mutex, numPhil)
 	for i := 0; i < numPhil; i++ {
-		fmt.Printf("Philosopher #%d\n", philosophers[i])
+		forks[i] = &sync.Mutex{}
 	}
+
+	philosophers := make([]*Philosopher, numPhil)
+	for i := 0; i < numPhil; i++ {
+		philosophers[i] = &Philosopher{
+			id:        i + 1,
+			leftFork:  forks[i],
+			rightFork: forks[(i+1)%numPhil],
+		}
+
+	}
+	return philosophers
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	philosophers := createPhilArr(numPhil)
+
+	for _, philosopher := range philosophers {
+		wg.Add(1)
+		go philosopher.eatFood(&wg)
+	}
+
+	wg.Wait()
+	fmt.Println("All food has been eaten. Map is below.\n")
+	fmt.Printf("Food Map: \n %d", m)
+
 }
